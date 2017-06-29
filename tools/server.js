f74882315c89b36dae56066ebb72773d4e8be800
Valentin Voilean <valentin@lodgify.com>
2017-06-29 13:25:13 +0200

    added .editorconfig file, load the normalize.css directly from node_modules

--- tools/server.js
+++ tools/server.js
@@ -5,22 +5,21 @@ const bs = require('browser-sync').create();
 
 const buildSass = require('./build');
 
-bs.watch('src/*.scss', function (event, file) {
-    if (event === 'change') {
-        buildSass(file);
-    }
+bs.watch('src/*.scss', (event, file) => {
+  if (event === 'change') {
+    buildSass(file);
+  }
 });
 
-// Listen to change events on HTML and reload
 bs.watch('public/**/*.css').on('change', bs.reload);
 bs.watch('public/**/*.html').on('change', bs.reload);
 
-// Now init the Browsersync server
 bs.init({
-    server: {
-        baseDir: 'src',
-        routes: {
-            '/css': 'public/css'
-        }
+  server: {
+    baseDir: 'src',
+    routes: {
+      '/css': 'public/css',
+      '/node_modules': 'node_modules'
     }
+  }
 });
